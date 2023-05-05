f5d59be3a47438fb94e863fb83dcefa5d3eaf3b1
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-05-05 17:58:31 +0530

    Add: lifecycle to CodeBuild bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,21 +3,16 @@
 #----
 
 resource "aws_s3_bucket" "codebuild" {
-  count = var.codebuild_s3_logs ? 1 : 0
+  count = var.enable_codebuild_s3_logs ? 1 : 0
 
   bucket        = "${module.this.id}-codebuild-log"
-  force_destroy = var.cb_log_bucket_force_destroy
+  force_destroy = var.codebuild_log_bucket_force_destroy
 
-  tags = merge(
-    module.this.tags,
-    {
-      Name = "${module.this.id}-codebuild"
-    }
-  )
+  tags = module.this.tags
 }
 
 resource "aws_s3_bucket_ownership_controls" "codebuild" {
-  count = var.codebuild_s3_logs ? 1 : 0
+  count = var.enable_codebuild_s3_logs ? 1 : 0
 
   bucket = aws_s3_bucket.codebuild[0].id
 
@@ -27,13 +22,32 @@ resource "aws_s3_bucket_ownership_controls" "codebuild" {
 }
 
 resource "aws_s3_bucket_public_access_block" "codebuild" {
-  count = var.codebuild_s3_logs ? 1 : 0
+  count = var.enable_codebuild_s3_logs ? 1 : 0
 
   bucket                  = aws_s3_bucket.codebuild[0].id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+  
+resource "aws_s3_bucket_lifecycle_configuration" "codebuild_log" {
+  count      = var.create_codebuild_log_bucket_lifecycle ? 1 : 0
+
+  bucket = aws_s3_bucket.codebuild[0].id
+
+  rule {
+    id = "expiration-${var.codebuild_log_bucket_lifecycle_expiration_days}"
+
+    status = "Enabled"
+
+    transition {
+      days          = var.codebuild_log_bucket_lifecycle_transition_days
+      storage_class = "GLACIER"
+    }
+    expiration {
+      days = var.codebuild_log_bucket_lifecycle_expiration_days
+    }
 }
 
 #----
@@ -42,18 +56,12 @@ resource "aws_s3_bucket_public_access_block" "codebuild" {
 
 resource "aws_s3_bucket" "codepipeline" {
   bucket        = "${module.this.id}-artifacts"
-  force_destroy = var.cp_artifact_bucket_force_destroy
+  force_destroy = var.codepipeline_artifact_bucket_force_destroy
 
-  tags = merge(
-    module.this.tags,
-    {
-      Name = "${module.this.id}-artifacts"
-    }
-  )
+  tags = module.this.tags
 }
 
 resource "aws_s3_bucket_ownership_controls" "codepipeline" {
-
   bucket = aws_s3_bucket.codepipeline.id
 
   rule {
